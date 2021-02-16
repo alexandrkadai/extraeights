fea594a71ea407c78d9e5fe853f722ac2c6d2843
alexK <alexandrkadai@gmail.com>
2021-02-16 13:17:45 +0200

    start

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+
+
+    //let windy = document.documentElement.clientWidth;
+    //alert(windy);
+    
+    function same(){
+        let arr = document.querySelectorAll('.element');
+       // alert(arr.length);
+        arr.forEach(function(item) {
+          //  alert(item);   
+                item.classList.add("ani");
+        } )
+    };
+    
+    same();
+    //let chi = document.getElementById("element1");
+  //  increase style.left by 2px;
+//});
+
